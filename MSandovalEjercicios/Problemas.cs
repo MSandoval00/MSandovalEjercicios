2d00988913014a5592c379253fd6d8c4f4845a94
MSandoval00 <msandovalg00@gmail.com>
2024-01-29 18:42:39 -0600

    Add project files.

---
+++ MSandovalEjercicios/Problemas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSandovalEjercicios
+{
+    public class Problemas
+    {
+        public void Ejerci1()
+        {
+            string[] listaA = new string[] { "A", "B", "C", "D", "E", "F" };
+            string[] listaB = new string[] { "B", "E", "G", "H", "I", "J" };
+            List<string> listaC = new List<string>();
+
+            listaC.AddRange(listaA);
+            listaC.AddRange(listaB);
+            string[] listaC2 = listaC.ToArray();
+            foreach (var item in listaC2)
+            {
+                Console.Write(item+" ");
+                Console.WriteLine("");
+               
+            }
+        }
+        //public void Ejercicio2()
+        //{
+
+        //}
+        public void Ejercicio3()
+        {
+            string palabra = "";
+            Console.WriteLine("Escribe la palabra para convertir a palindroma");
+            palabra=Console.ReadLine();
+            
+            char[] cadenaInvertida=palabra.ToCharArray();
+            Array.Reverse(cadenaInvertida);
+            
+                if (palabra==cadenaInvertida)
+                {
+                    Console.WriteLine("La palabra es palindromo");
+                }
+                else
+                {
+                    Console.WriteLine("No es palindromo");
+                }
+            
+        }
+       
+        
+    }
+}
---
+++ MSandovalEjercicios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSandovalEjercicios
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Problemas obj1=new Problemas();
+            obj1.Ejerci1();
+            //Console.ReadKey();
+            
+            //Problemas obj2=new Problemas();
+            //obj2.Ejercicio2();
+            //Console.ReadKey();
+            Problemas obj3=new Problemas();
+            obj3.Ejercicio3();
+            Console.ReadKey();
+        }
+    }
+}
