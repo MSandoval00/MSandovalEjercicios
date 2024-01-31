7876aad26a29462f5749c3dfd6941094aafabcfd
MSandoval00 <msandovalg00@gmail.com>
2024-01-30 18:38:00 -0600

    Problemas

--- MSandovalEjercicios/Problemas.cs
+++ MSandovalEjercicios/Problemas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,9 @@ namespace MSandovalEjercicios
             string[] listaC2 = listaC.ToArray();
             foreach (var item in listaC2)
             {
-                Console.Write(item+" ");
+                Console.Write(item + " ");
                 Console.WriteLine("");
-               
+
             }
         }
         //public void Ejercicio2()
@@ -30,24 +31,111 @@ namespace MSandovalEjercicios
         //}
         public void Ejercicio3()
         {
+
             string palabra = "";
-            Console.WriteLine("Escribe la palabra para convertir a palindroma");
-            palabra=Console.ReadLine();
+            char[] cadenaInvertida;
+            char[] oracion;
+            char[] oracionInvertida;
+
+            Console.WriteLine("Escribe la palabra para convertir a palindromo");
+            palabra = Console.ReadLine().ToLower().Replace(" ", "");
+
             
-            char[] cadenaInvertida=palabra.ToCharArray();
-            Array.Reverse(cadenaInvertida);
-            
-                if (palabra==cadenaInvertida)
+            cadenaInvertida = palabra.ToCharArray();
+            for (int i = 0; i < palabra.Length/2; i++)
+            {
+                char ch = cadenaInvertida[i];
+                cadenaInvertida [i] = cadenaInvertida[palabra.Length-i-1];
+                cadenaInvertida[palabra.Length - i - 1] = ch;
+            }
+            //Array.Reverse(cadenaInvertida);
+
+            char[] palabraInvertida = cadenaInvertida; 
+
+            char[] arregloPalabra = palabra.ToCharArray();
+            for (int i = 0; i < arregloPalabra.Length; i++)
+            {
+                arregloPalabra[i] = palabra[i];
+                oracion = arregloPalabra;
+                for (int j = 0; j < palabraInvertida.Length; j++)
                 {
-                    Console.WriteLine("La palabra es palindromo");
+                    palabraInvertida[j] = palabraInvertida[j];
+                   oracionInvertida = palabraInvertida;
                 }
-                else
+                
+            }
+            if (palabraInvertida.SequenceEqual(arregloPalabra))
+            {
+                Console.WriteLine("Es Palindromo");
+            }
+            else
+            {
+                Console.WriteLine("No es palindromo");
+            }
+
+        }
+        public void Ejercicio4()
+        {
+            int numero=0;
+            List<int> pares = new List<int>();
+            List<int> impares = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%3==0)
                 {
-                    Console.WriteLine("No es palindromo");
+
+                    pares.Add(i);
                 }
+                if (i%2==0)
+                {
+                    impares.Add(i);
+                } 
+            }
+            Console.Write("Lista de numero impares son: ");
+            foreach (var itemPares in pares)
+            {
+                Console.Write(" " + itemPares + " ");
+            }
+            Console.WriteLine("\n");
+            Console.Write("Lista de numero pares son: ");
+            foreach (var itemImpares in impares)
+            {
+                Console.Write(" " + itemImpares + " ");
+            }
             
         }
-       
-        
+        public void Ejercicio5()
+        {
+            int numeroProducto=0;
+            decimal precioProducto = 0;
+            List<object> ListaPrecio = new List<object>();
+            
+
+            Console.WriteLine("¿Cuántos productos quieres registrar?");
+            numeroProducto = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numeroProducto; i++)
+            {
+                Console.WriteLine("Ingresa el precio del producto: " + i);
+                precioProducto = decimal.Parse(Console.ReadLine());
+                var productos = new
+                {
+                    Numero = i,
+                    precioProducto = precioProducto,
+                };
+                
+                ListaPrecio.Add(productos);
+
+                
+                foreach (var itemProducto in ListaPrecio)
+                {
+                    dynamic resultJSON=JObject.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(itemProducto));
+                    
+                }
+
+            }
+            
+        }
+
     }
 }
--- MSandovalEjercicios/Program.cs
+++ MSandovalEjercicios/Program.cs
@@ -10,15 +10,21 @@ namespace MSandovalEjercicios
     {
         static void Main(string[] args)
         {
-            Problemas obj1=new Problemas();
-            obj1.Ejerci1();
-            //Console.ReadKey();
+            //Problemas obj1=new Problemas();
+            //obj1.Ejerci1();
+            
             
             //Problemas obj2=new Problemas();
             //obj2.Ejercicio2();
             //Console.ReadKey();
-            Problemas obj3=new Problemas();
-            obj3.Ejercicio3();
+            //Problemas obj3=new Problemas();
+            //obj3.Ejercicio3();
+            //Problemas obj4=new Problemas();
+            //obj4.Ejercicio4();
+
+            Problemas obj5=new Problemas();
+            obj5.Ejercicio5();
+
             Console.ReadKey();
         }
     }
